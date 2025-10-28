9c7cb306cfde456642a39c66acf8f341b7eb4b42
Irene Biju <imb21@duke.edu>
2025-10-28 12:26:36 -0400

    Added purchases button and images

--- app/models/purchase.py
+++ app/models/purchase.py
@@ -30,3 +30,37 @@ ORDER BY time_purchased DESC
                               uid=uid,
                               since=since)
         return [Purchase(*row) for row in rows]
+
+
+    @staticmethod
+    def get_all_detailed_by_uid(uid):
+        """
+        Return a user's purchase history, including product details and user name.
+        """
+        rows = app.db.execute('''
+SELECT p.id AS purchase_id,
+       p.uid AS user_id,
+       u.firstname || ' ' || u.lastname AS user_name,
+       pr.name AS product_name,
+       pr.price AS product_price,
+       pr.image_link,
+       p.time_purchased
+FROM Purchases p
+JOIN Users u ON p.uid = u.id
+JOIN Products pr ON p.pid = pr.id
+WHERE p.uid = :uid
+ORDER BY p.time_purchased DESC
+''', uid=uid)
+
+        result = []
+        for row in rows:
+            result.append({
+                "purchase_id": row[0],
+                "user_id": row[1],
+                "user_name": row[2],
+                "product_name": row[3],
+                "product_price": row[4],
+                "image_url": row[5],
+                "time_purchased": row[6]
+            })
+        return result
