3e6d45e53146ef0e8bad8e1c6b52cbdec9efd1f2
Ishan Madan <im121@duke.edu>
2025-11-25 10:55:01 -0500

    allow missing DB_PASSWORD in dev environments by defaulting to empty string

--- app/config.py
+++ app/config.py
@@ -4,9 +4,10 @@ from urllib.parse import quote_plus
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    # Allow missing DB_PASSWORD in dev environments by defaulting to empty string
     SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'\
         .format(os.environ.get('DB_USER'),
-                quote_plus(os.environ.get('DB_PASSWORD')),
+                quote_plus(os.environ.get('DB_PASSWORD') or ''),
                 os.environ.get('DB_HOST'),
                 os.environ.get('DB_PORT'),
                 os.environ.get('DB_NAME'))
